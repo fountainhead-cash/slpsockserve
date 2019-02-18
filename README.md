dbdfbf4af8946877e63b4b30103c334e34058e47
blockparty <hello@blockparty.sh>
2019-02-17 22:22:39 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+sockserve
+===
+
+Sockserve is a Bitserve-like application but designed for Bitsocket.
+
+It requires you to already be running [bitsocket](https://github.com/fountainhead-cash/bitsocket) and a [bitd](https://github.com/fountainhead-cash/bitd) server.
+
+## setup
+
+```
+npm install
+cp .env.example .env
+$(EDITOR) .ev
+npm start
+```
---
+++ index.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+const express = require('express')
+const ip = require('ip')
+const app = express()
+const cors = require("cors")
+
+const config = {
+  "query": {
+    "v": 3,
+    "q": { "find": {}, "limit": 10 },
+    "r": {
+      "f": "[.[] | .out[] | .str]"
+     }
+  },
+  "url": process.env.bitsocket_url ? process.env.bitsocket_url : "http://127.0.0.1:3000/s/",
+  "port": Number.parseInt(process.env.sockserve_port ? process.env.sockserve_port : 3005)
+};
+
+var db
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(cors())
+app.enable("trust proxy")
+app.get(/^\/channel\/(.+)/, function(req, res) {
+  let encoded = req.params[0]
+  let decoded = Buffer.from(encoded, 'base64').toString()
+  res.render('channel', {
+    bitserve_url: config.url,
+    code: decoded
+  })
+});
+app.get('/channel', function (req, res) {
+  res.render('channel', {
+    bitserve_url: config.url,
+    code: JSON.stringify(config.query, null, 2)
+  })
+});
+app.get('/', function(req, res) {
+  res.redirect('/channel')
+});
+app.listen(config.port, () => {
+  console.log("######################################################################################");
+  console.log("#")
+  console.log("#  SOCKSERVE: Bitsocket Microservice")
+  console.log("#  Serving Transactions through HTTP...")
+  console.log("#")
+  console.log(`#  Channel: ${ip.address()}:${config.port}/channel`);
+  console.log("#")
+  console.log("#  Learn more at https://docs.fountainhead.cash")
+  console.log("#")
+  console.log("######################################################################################");
+  }
+)
