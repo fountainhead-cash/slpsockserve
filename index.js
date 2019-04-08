50d2849a1fe703ce980d944d39900908624a4a98
blockparty <hello@blockparty.sh>
2019-04-08 16:31:25 -0500

    use new fountainhead-core

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const ip = require('ip')
 const app = express()
 const cors = require("cors")
-const slpsocketd = require('fountainhead-slpsocketd')
+const slpsocketd = require('fountainhead-core').slpsocketd
 
 const config = {
   "query": {
